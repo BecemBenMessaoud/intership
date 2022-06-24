87db81543c29934ae8a014392f93dac1df705ca8
Becem BenM <BenMessaoudbecem93@gmail.com>
2022-06-24 10:50:11 +0100

    modules fixed

---
+++ role-and-permission/role-and-permission-common/src/main/java/com/proxym/Main.java
@@ -0,0 +1,9 @@
+package com.proxym;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
+
---
+++ role-and-permission/role-and-permission-management/src/main/java/com/proxym/Main.java
@@ -0,0 +1,7 @@
+package com.proxym;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
